d3d4c0220687c7059c3f8c1d1a46994ae1fe007f
unknown <pablo escalante>
2022-09-27 20:18:45 -0500

    commit final

--- src/main/java/pe/isil/moduloseguridad/ModuloseguridadApplication.java
+++ src/main/java/pe/isil/moduloseguridad/ModuloseguridadApplication.java
@@ -3,6 +3,9 @@ package pe.isil.moduloseguridad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.Scanner;
 import java.util.logging.Logger;
@@ -10,27 +13,12 @@ import java.util.logging.Logger;
 @SpringBootApplication
 public class ModuloseguridadApplication {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 		SpringApplication.run(ModuloseguridadApplication.class, args);
 
 
-		/*//Cargar Driver
-		Class.forName("com.mysql.cj.jdbc.Driver");
+		// LISTAR REGISTROS - STATEMENT
 
-		//Crear conexión
-		Connection connexion = DriverManager
-				.getConnection("jdbc:mysql://localhost:3306/ep1",
-						"root","root");
-
-		//Crear Statement
-		Statement statement = connexion.createStatement();
-
-		// Ejecutar sentencia
-		int afectedRows = statement
-				.executeUpdate("UPDATE USERS SET name = 'jose' where id=1");*/
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("Ingresa la entrada: ");
-		int id = Integer.parseInt(entrada.next());
 		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		try {
@@ -39,16 +27,86 @@ public class ModuloseguridadApplication {
 							"root","root");
 			Statement st = connexion.createStatement();
 			ResultSet rs = st
-					.executeQuery("select * from empleado where id =" + id);
+					.executeQuery("select * from empleado");
 			while(rs.next()){
 				String nombre = rs.getString("nombre");
 				String apellido = rs.getString("apellido");
 				String pais = rs.getString("pais");
 				System.out.println("Empleado: " + nombre + " " + apellido +
-						"del país: " + pais);
+						" del país: " + pais);
 			}
 			rs.close();
 			st.close();
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		// ELIMINAR REGISTROS - PREPAREDSTATEMENT
+
+		try {
+
+			Scanner sc = new Scanner(System.in);
+
+			System.out.println("Por favor el id del empleado que quiere borrar");
+
+			String id = sc.nextLine();
+
+			Connection connexion = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/ep1",
+							"root","root");
+
+			PreparedStatement ps = connexion
+					.prepareStatement("delete from empleado where id="+id);
+			int vañ = ps.executeUpdate();
+
+			System.out.println("Se ha eliminado el empleado");
+
+			ps.close();
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		// CREAR REGISTROS - CALLABLESTATEMENT
+
+		try {
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.println("Por favor ingrese su nombre");
+
+			String nombre = br.readLine();
+
+			System.out.println("Ingrese su apellido");
+
+			String apellido = br.readLine();
+
+			System.out.println("Ingrese su pais");
+
+			String pais = br.readLine();
+
+			br.close();
+
+			Connection connexion = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/ep1",
+							"root","root");
+
+
+
+			CallableStatement cs = connexion
+					.prepareCall("{call insertar_empleado('" +
+							nombre+"','" +
+							apellido+"','" +
+							pais+"')}");
+
+
+
+			ResultSet rs = cs
+					.executeQuery();
+
+			System.out.println("Se han ingresado los datos");
+
+			rs.close();
+			cs.close();
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
